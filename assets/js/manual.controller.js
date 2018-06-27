dad9708197d22d4143a2514dea801de245e7d54e
Heloisa Neto <heloisa.neto.ext@siemens.com>
2018-06-27 22:22:18 +0100

    test - artyom Listen forever

--- assets/js/manual.controller.js
+++ assets/js/manual.controller.js
@@ -24,25 +24,25 @@
       vm.manualRequest = true;
 
       vm.artyom = new Artyom();
-      // vm.artyom.addCommands([{
-      //     description: "Artyom can talk too, lets say something if we say hello",
-      //     indexes: ["hello", "hey"],
-      //     action: function (i) {
-      //       if (i == 0) {
-      //         vm.artyom.say("Hello! How are you?");
-      //       }
-      //     }
-      //   },
-      //   {
-      //     indexes: ["goodbye"],
-      //     action: function () {
-      //       vm.artyom.say("bye");
-      //     }
-      //   }
-      // ]);
+      vm.artyom.addCommands([{
+          description: "Artyom can talk too, lets say something if we say hello",
+          indexes: ["hello", "hey"],
+          action: function (i) {
+            if (i == 0) {
+              vm.artyom.say("Hello! How are you?");
+            }
+          }
+        },
+        {
+          indexes: ["goodbye"],
+          action: function () {
+            vm.artyom.say("bye");
+          }
+        }
+      ]);
 
       vm.artyom.redirectRecognizedTextOutput(function (text, isFinal) {
-        
+
 
         // if (isFinal) {
         //   span.innerHTML = '';
@@ -55,16 +55,21 @@
 
     }
 
-    $scope.startArtyom = function() {
+    $scope.startArtyom = function () {
       vm.artyom.initialize({
         lang: "en-GB",
-        continuous: false,
+        continuous: true, // Listen forever
+        soundex: true, // Use the soundex algorithm to increase accuracy
         debug: true,
         listen: true
+      }).then(() => {
+        console.log("Artyom has been succesfully initialized");
+      }).catch((err) => {
+        console.error("Artyom couldn't be initialized: ", err);
       });
     }
 
-    $scope.stopArtyom = function() {
+    $scope.stopArtyom = function () {
       vm.artyom.fatality();
     }
   }
